c258017cc960918c8df24d570620c641f51c5587
NagoySit <danic12tsar@gmail.com>
2024-06-08 20:48:25 +0300

    ChangeTask#7

--- src/main/resources/db/changelog/sql/2024-05-07_20-25_CreateTablesForProduct.sql
+++ src/main/resources/db/changelog/sql/2024-05-07_20-25_CreateTablesForProduct.sql
@@ -1,18 +1,19 @@
 -- liquibase formatted sql
--- changeset Dan Tsarenko:2024-05-07_20-30_1
-CREATE TABLE IF NOT EXISTS "product_info" (
-   id BIGSERIAL PRIMARY KEY,
-   product_id BIGSERIAL UNIQUE NOT NULL,
-   calories INTEGER NOT NULL,
-   proteins INTEGER NOT NULL,
-   fats INTEGER NOT NULL,
-   carbohydrates INTEGER NOT NULL
-)
--- changeset Dan Tsarenko:2024-05-07_20-35_2
+-- changeset Dan Tsarenko:2024-05-08_20-40_1
 CREATE TABLE IF NOT EXISTS "product"(
    id BIGSERIAL PRIMARY KEY,
-   name VARCHAR(256) UNIQUE NOT NULL,
+   name VARCHAR(256)  NOT NULL,
    description TEXT NOT NULL,
    price NUMERIC(10,2) NOT NULL,
    amount INTEGER NOT NULL
+)
+-- changeset Dan Tsarenko:2024-05-08_20-42_2
+CREATE TABLE IF NOT EXISTS "product_info" (
+   id BIGSERIAL PRIMARY KEY,
+   product_id BIGINT UNIQUE NOT NULL,
+   calories INTEGER NOT NULL,
+   proteins INTEGER NOT NULL,
+   fats INTEGER NOT NULL,
+   carbohydrates INTEGER NOT NULL,
+   FOREIGN KEY (product_id) REFERENCES product(id)
 )
